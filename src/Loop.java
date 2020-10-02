18803a8cadf911d153c0f5dc3ccc5f8a56237782
yoon4027 <yooooonus@gmail.com>
2020-10-02 06:28:23 +0500

    Some Basic java

---
+++ src/Loop.java
@@ -0,0 +1,9 @@
+public class Loop {
+    public static void LoopCode(String str) {
+        int number = 5;
+        while (number < 50) {
+            System.out.println(str);
+            number++;
+        }
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main() {
+        try {
+            Loop loop_code = new Loop();
+            loop_code.LoopCode("Hello World.");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
